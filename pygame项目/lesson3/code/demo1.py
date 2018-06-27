1bfd01abeff460a914840cb3f5c24107150b0361
qianxun1314 <1992114725@qq.com>
2018-06-27 10:25:25 +0800

    18-6-27

---
+++ pygame项目/lesson3/code/demo1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/27 09:57
+# @Author  : 千と千寻
+# @Email   : pp2677345028@gmail.com
+# @File    : demo1.py
+
+import pygame
+from pygame.locals import *
+import time
+pygame.init()
+
+screen = pygame.display.set_mode((350, 500))
+pygame.display.set_caption("Author：千与千寻")
+ball = pygame.image.load("ball.png")
+bg = pygame.image.load("bg.jpg")
+ball_x = 0
+ball_y = 0
+
+font = pygame.font.SysFont("arial", 40)
+
+is_running = True
+
+while is_running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+
+
+    screen.fill((200, 100, 100))
+
+    screen.blit(bg, (0, 0))
+    screen.blit(ball, (ball_x, ball_y))
+    pygame.display.update()
